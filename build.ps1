792b4ebae828dea8ebd09a61f1fced87793799f4
patataofcourse <patataofcourse@gmail.com>
2022-12-15 18:57:10 +0100

    tickompiler hates me

--- build.ps1
+++ build.ps1
@@ -9,3 +9,5 @@ if ($? -eq 0) {
 cp -force tempo/* bin
 & $java -jar ..\tickompiler.jar p bin\ base.bin
 mv -force C00.bin $Env:APPDATA\Citra\sdmc\rhmm
+
+citra
--- build.sh
+++ build.sh
@@ -1,10 +1,14 @@
-if which citra > /dev/null; then
-    CITRA = ~/.config/citra-emu
+if which citra >&2 2> /dev/null; then
+    CITRA_DIR=~/.config/citra-emu
+    CITRA=citra
 else # assuming flatpak
-    CITRA = ~/.var/app/org.citra_emu.citra/data/citra-emu
+    CITRA_DIR=~/.var/app/org.citra_emu.citra/data/citra-emu
+    CITRA="flatpak run org.citra_emu.citra"
 fi
 
 java -jar ../tickompiler.jar c tickflow bin
-cp -f tempo/* bin
+cp -rf tempo/* bin/
 java -jar ../tickompiler.jar p bin base.bin
-mv -f C00.bin $CITRA/sdmc/rhmm
+cp -f C00.bin $CITRA_DIR/sdmc/rhmm
+
+$CITRA
